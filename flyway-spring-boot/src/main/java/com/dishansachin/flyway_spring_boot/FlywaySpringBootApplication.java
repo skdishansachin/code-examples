19d0ec0ffad48262b8916595d40bfa6f4a7ba6c4
skdishansachin <skdishansachin@gmail.com>
2025-04-22 21:38:22 +0530

    create new spring boot project with flyway

---
+++ flyway-spring-boot/src/main/java/com/dishansachin/flyway_spring_boot/FlywaySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dishansachin.flyway_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywaySpringBootApplication.class, args);
+	}
+
+}
---
+++ flyway-spring-boot/src/test/java/com/dishansachin/flyway_spring_boot/FlywaySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishansachin.flyway_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
